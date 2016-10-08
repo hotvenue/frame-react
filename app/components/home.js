b1548a07a13aa8bb93b4bc2c585da6f472d0918a
Niccolò Olivieri <niccolo@olivieriachille.com>
2016-10-08 22:59:30 +0200

    Added the first, basic, fetch

--- app/components/home.js
+++ app/components/home.js
@@ -3,10 +3,39 @@
 import React, { Component } from 'react';
 import { StyleSheet, Image, Text } from 'react-native';
 
+import Spinner from 'react-native-loading-spinner-overlay';
+
 export default class Home extends Component {
+  state: {
+    isLoading: boolean,
+    name: string,
+  };
+
+  constructor() {
+    super();
+
+    const identifierForVendor = 'FCA5A7BD-7E4B-4E37-A051-A8D8FC51D6EE';
+
+    this.state = {
+      isLoading: true,
+      name: '',
+    };
+
+    fetch(`http://api.hotvenueapp.com/device?identifierForVendor=${identifierForVendor}`)
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          isLoading: false,
+          name: res[0].name,
+        });
+      });
+  }
+
   render() {
     return (
-      <Image source={require('../assets/img/home/bg.png')} style={styles.container} />
+      <Image source={require('../assets/img/home/bg.png')} style={styles.container}>
+        <Spinner visible={this.state.isLoading}/>
+      </Image>
     );
   }
 }
